406e982f9c4072bd7cfad604102330dfc3ca9ceb
Glitch7788 <glitch7788@gmail.com>
2016-01-19 22:10:24 +0000

    Update Stringy.py

--- Stringy.py
+++ Stringy.py
@@ -1,1 +1,12 @@
-
+def stringy(size):
+    counter = 1
+    string = ''
+    while counter < len(size) + 1: #While counter less than length of string entered
+        if counter % 2 == 0: #if counter is even
+            string = string + '0' #string becomes string concatenated with 0
+        else:
+            string = string + '1' #string becomes string concatenated with 1
+        counter += 1
+    
+    print(string)
+stringy('forthelulz')

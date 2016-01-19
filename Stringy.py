272beea73e920a580a1d71c3574ddb98d6dc4859
Glitch7788 <glitch7788@gmail.com>
2016-01-19 22:11:53 +0000

    Update Stringy.py

--- Stringy.py
+++ Stringy.py
@@ -3,9 +3,9 @@ def stringy(size):
     string = ''
     while counter < len(size) + 1: #While counter less than length of string entered
         if counter % 2 == 0: #if counter is even
-            string = string + '0' #string becomes string concatenated with 0
+            string += '0' #string becomes string concatenated with 0
         else:
-            string = string + '1' #string becomes string concatenated with 1
+            string += '1' #string becomes string concatenated with 1
         counter += 1
     
     print(string)

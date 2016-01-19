1d35d80abd028c13d1a8003a2e5db653c4e27bb8
Glitch7788 <glitch7788@gmail.com>
2016-01-19 22:59:50 +0000

    Update BrokenGreetings.py

--- BrokenGreetings.py
+++ BrokenGreetings.py
@@ -2,11 +2,11 @@
 # Good luck all!
 
 class Person:
-  def __init__(self, name):
-    self.name = name
+    def __init__(self, name):
+        self.name = name
   
-  def greet(self, other_name):
-    return "Hi {0}, my name is {1}".format(other_name, name)
+    def greet(self, other_name):
+        return "Hi {0}, my name is {1}".format(other_name, name)
 
 # Expected outcome:
 # 'Hi Kate, my name is Joe'
